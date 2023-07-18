35fbaebbae800c0c55dffb87f31d60d9e04c7505
Mateo Kladarić <52569856+MrFacysus@users.noreply.github.com>
2023-07-18 06:20:53 +0200

    Add files via upload

---
+++ PPM.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Pope_Process_Monitor
+{
+    partial class PPM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.materialCheckedListBox1 = new MaterialSkin.Controls.MaterialCheckedListBox();
+            this.SuspendLayout();
+            // 
+            // materialCheckedListBox1
+            // 
+            this.materialCheckedListBox1.AutoScroll = true;
+            this.materialCheckedListBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.materialCheckedListBox1.Depth = 0;
+            this.materialCheckedListBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.materialCheckedListBox1.Location = new System.Drawing.Point(3, 64);
+            this.materialCheckedListBox1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialCheckedListBox1.Name = "materialCheckedListBox1";
+            this.materialCheckedListBox1.Size = new System.Drawing.Size(794, 383);
+            this.materialCheckedListBox1.Striped = false;
+            this.materialCheckedListBox1.StripeDarkColor = System.Drawing.Color.Empty;
+            this.materialCheckedListBox1.TabIndex = 0;
+            this.materialCheckedListBox1.Click += new System.EventHandler(this.materialCheckedListBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.materialCheckedListBox1);
+            this.Name = "Form1";
+            this.Text = "Pope Process Monitor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialCheckedListBox materialCheckedListBox1;
+    }
+}
+
---
+++ PPM.cs
@@ -0,0 +1,88 @@
+﻿using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pope_Process_Monitor
+{
+    public partial class PPM : MaterialForm
+    {
+        public PPM()
+        {
+            InitializeComponent();
+        }
+
+        public List<string> ActiveWatchers = new List<string>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (System.Diagnostics.Process p in System.Diagnostics.Process.GetProcesses())
+            {
+                if (!materialCheckedListBox1.Items.Where(x => x.Text == p.ProcessName).Any())
+                {
+                    materialCheckedListBox1.Items.Add(p.ProcessName);
+                }
+            }
+
+            Task Watcher = new Task(() =>
+            {
+                while (true)
+                {
+                    foreach (System.Diagnostics.Process p in System.Diagnostics.Process.GetProcesses())
+                    {
+                        if (!materialCheckedListBox1.Items.Where(x => x.Text == p.ProcessName).Any())
+                        {
+                            materialCheckedListBox1.Invoke(new Action(() =>
+                            {
+                                materialCheckedListBox1.Items.Add(p.ProcessName);
+                            }));
+                        }
+                    }
+
+                    foreach (var item in materialCheckedListBox1.Items.Where(x => x.Checked))
+                    {
+                        if (!ActiveWatchers.Where(x => x.Split('\\').Last().Split('.').First() == item.Text).Any())
+                        {
+                            Invoke(new Action(() =>
+                            {
+                                foreach (var proc in System.Diagnostics.Process.GetProcessesByName(item.Text))
+                                {
+                                    ActiveWatchers.Add(proc.MainModule.FileName);
+                                }
+                            }));
+                        }
+                        else if (!materialCheckedListBox1.Items.Where(x => x.Text == item.Text).Any())
+                        {
+                            ActiveWatchers.Remove(item.Text);
+                        }
+                    }
+
+                    foreach (var item in ActiveWatchers)
+                    {
+                        if (!System.Diagnostics.Process.GetProcessesByName(item.Split('\\').Last().Split('.').First()).Any())
+                        {
+                            System.Diagnostics.Process.Start(item);
+                        }
+                        else
+                        {
+                            if (System.Diagnostics.Process.GetProcessesByName(item.Split('\\').Last().Split('.').First()).Where(x => x.Responding == false).Any())
+                            {
+                                System.Diagnostics.Process.GetProcessesByName(item.Split('\\').Last().Split('.').First()).Where(x => x.Responding == false).First().Kill();
+                            }
+                        }
+                    }
+
+                    System.Threading.Thread.Sleep(1000);
+                }
+            });
+            Watcher.Start();
+        }
+
+        private void materialCheckedListBox1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
